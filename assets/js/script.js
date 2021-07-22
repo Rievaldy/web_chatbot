4a4b22acee990a4745ef9ce59edb10bd683bb656
Ujang <jabbarujang@gmail.com>
2021-07-22 16:06:45 +0700

    remove add wkwko

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
+$('.item-purchase-banner').remove()
+// $('.purchase-banner-active.header-fixed .t-header').css('top', '0')
+
 $(document).ready( function () {
+
     $('.i-datatable').DataTable({
         dom: 'Bfrtip',
         buttons: [
