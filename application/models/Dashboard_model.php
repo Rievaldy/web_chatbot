475125ae1647361a7565459dcedc7ef9a91fc275
Ujang <jabbarujang@gmail.com>
2021-07-23 23:07:46 +0700

    fix dashboard

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -4,7 +4,8 @@ class Dashboard_model extends CI_Model {
 
     public function index()
     {
-        $sql = "SELECT (SELECT 25) AS request_maintenance, (SELECT 225) AS chat, (SELECT count(id_tag)
+        $sql = "SELECT (SELECT count(*) FROM maintenance) AS request_maintenance, 
+        (SELECT count(*) FROM chat_history) AS chat, (SELECT count(id_tag)
         FROM intents) AS label, (SELECT count(response)
         FROM intents) AS answer, (SELECT count(id_pattern)
         FROM patterns) AS question";
