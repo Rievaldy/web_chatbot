ba0c56e136a826b1766d169abe78cb3d376fc183
Rievaldy <rievaldy3235@gmail.com>
2021-08-02 20:02:27 +0700

    nice ass

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/';
+$config['base_url'] = 'http://localhost/web_chatbot/';
 // $config['base_url'] = 'https://042fc5655c3a.ngrok.io';
 
 /*
